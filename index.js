1a13ee18a2d78555cb2c0087a4ad84d9164d4e68
racie7 <graciamuts@gmail.com>
2022-08-13 16:14:51 +0300

    Done

---
+++ index.js
@@ -0,0 +1,16 @@
+function divide(a,b){
+  let quotient
+  quotient = a/b;
+
+  return quotient;
+}
+
+const square = (number) => {
+    return number ** 2;
+}
+
+const add = (num1, num2) => {
+    return num1 + num2;
+}
+  
+  
